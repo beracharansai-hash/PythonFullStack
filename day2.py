a474c60523ad79ce39517e9dcee17dcc04d4e162
beracharansai-hash <beracharansai@gmail.com>
2026-05-14 15:49:04 +0530

    Day 2 of Learning Python

---
+++ day2.py
@@ -0,0 +1,124 @@
+'''
+a=9
+b=7
+print(a==b)
+
+operators
+-----------
+1.ARTHEMATIC
+------------
++,-,*,/,//,**
+------------
+
+2.ASSIGNMENT
+------------
+==,+=,-=,>=,%=,*=
+------------
+
+3.COMPARISION
+------------
+*==,!=,>=,<=,>,<
+------------
+4.LOGICAL
+------------
+AND(BOTH CONDITIONS SHOULD BE TRUE)
+OR(ONE CONDITION SHOULD BE TRUE)
+NOT
+------------
+5.MEMBERSHIP
+------------
+
+6.IDENTITY
+------------
+IS AND IS NOT
+------------
+
+7.BITWISE
+
+
+A=5
+if (A%5==0) and (A%15==0):
+    print("true")
+    '''
+x = 10
+y = 20
+if x > 5 and y > 15:
+    print("Both conditions are True") # Output
+
+'''
+
+
+
+
+
+day2.py
+Data Types
+-------------
+String
+("")('')
+String is a sequence of char that are enclosed in ' '," "
+String is immutable
+-------------
+
+1)Methods
+-------------
+replace()
+--> Used to replace with new substring
+Syntax---->variable_name.replace("old string","new string")
+
+any="Python is a language"
+print(any.replace("Python","Java"))
+print(any)
+
+
+
+
+2)Split
+----------------
+--->used to seperate into different parts
+--->
+
+any="python is a language"
+print(any.split("is"))
+
+
+3)len()
+
+----------
+--> get number of items,substring
+Syntax--->len(variable_name)
+any="python is a language"
+print(len(any))
+
+
+4)Slicing()
+
+------------
+
+any="python is a language"
+print(any[3:13])
+
+
+5)indexing
+--------------
+--->
+
+'''
+any="python is a language"
+print(any[7])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
