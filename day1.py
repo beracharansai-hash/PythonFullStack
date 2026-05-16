a72e9b51f4e36a1024ea3da35696e3d779762bc5
beracharansai-hash <beracharansai@gmail.com>
2026-05-16 12:30:02 +0530

    Day 1 of learning python

---
+++ day1.py
@@ -0,0 +1,52 @@
+print("This is my first python class")
+print("Hello Sampath")
+'''
+what is Python?
+-High level language(No need to Worry about low level management(storage)
+-Dynamically Typed Language(No need to mention the kind of data taking to the variable)
+-Interpreter Language(python will execute the code line by line and can throw the error if
+ any but all the before line will execute and gives output
+ -------------------
+ What are Tokens?
+ Keywords in the Program
+ example:if,else,while,for,is etc.
+ -------------------
+ What are identifiers?
+ Name of variables,functions and classes
+ -------------------
+ What is Variables?
+ good ways(A-Z, a-z)
+ bad ways(special char,Spaces,0-9)
+ ------------------
+ Comments
+ (1.single line(#))
+ (2.Multi line (''''''')
+ Craete a variables
+
+
+name=("charan")
+age1=20
+branch=("cse")
+
+
+'''
+
+'''
+BAD variable
+
+1age=20
+ag e=24
+@age=25
+ age=28
+Good variable
+
+
+KFC_Viz_branch_emp_name=("Charan")
+KFC_VSP_branch_emp_name=("Sampath")
+KFC_GWK_branch_emp_name=("sumanth")
+'''
+
+
+
+
+ 
