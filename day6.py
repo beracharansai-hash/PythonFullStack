90a54995e6341e2bc83de1aebb6808ed504363c0
beracharansai-hash <beracharansai@gmail.com>
2026-05-19 15:04:07 +0530

    Day 6 of learning Python

---
+++ day6.py
@@ -0,0 +1,125 @@
+
+'''
+Type conversions
+------------------
+int---->
+
+an=78
+us=str(an)
+on=float(an)
+print(us)
+print(on)
+print(type(us))
+
+
+an="sai"
+ear=int(an)
+print(ear)
+
+an="90"
+ear=int(an)
+print(ear)
+
+string can be converted into int but only happens when the string
+consists of integer values in it
+
+an="90"
+ear=set(an)
+print(ear)
+
+
+an="90"
+ear=list(an)
+print(ear)
+
+
+an="90"
+ear=tuple(an)
+print(ear)
+
+
+an="90"
+ear=float(an)
+print(ear)
+
+car=95.4
+print(str(car))
+
+for j in str(car):
+    print(car)
+
+
+
+an="python"
+ear=tuple(an)
+print(ear)
+
+an=[1,2,3,4]
+so=str(an)
+print(so)
+for j in str(an):
+    print(j)
+
+
+an=(1,2,3,4)
+so=list(an)
+print(so)
+print(str(so))
+
+for j in str(so):
+    print(j)
+-------------------
+Int as a user-input
+-------------------
+num=int(input("Enter the number"))
+nun=int(input("enter the number"))
+print(num+nun)
+
+-------------------
+str as a user-input
+-------------------
+
+num=(input("Enter the value"))
+nun=(input("enter the value"))
+print(num+nun)
+
+
+
+any=list(map(int,input("enter the Value").split()))
+
+print(any)
+
+
+any=tuple(map(int,input("enter the Value").split(",")))
+
+print(any)
+'''
+num=eval(input("enter:"))
+print(eval)
+print(type(num))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
